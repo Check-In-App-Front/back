a2d0a5c1d0df98b0ce559f6987893e79e5051895
hjh5333 <50477554+hjh5333@users.noreply.github.com>
2024-03-19 00:08:14 +0900

    Add files via upload

---
+++ check-in-back/project/src/main/java/com/ace/project/ProjectApplication.java
@@ -0,0 +1,22 @@
+package com.ace.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+	
+	@GetMapping("/test")
+	public String hello(){
+		return "테스트입니다.";
+	}
+
+
+}
