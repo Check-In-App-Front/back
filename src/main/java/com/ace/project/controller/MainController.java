edb34cb8278da9ab8cc0036fade5d121502bc183
parkJi-yun <nmjk0123@naver.com>
2024-05-01 19:42:51 +0900

    Feat: swagger test

--- src/main/java/com/ace/project/controller/MainController.java
+++ src/main/java/com/ace/project/controller/MainController.java
@@ -3,9 +3,12 @@ package com.ace.project.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 public class MainController {
-    
+	
+	@Operation(summary = "test", description = "메인 페이지 test")
     @GetMapping("/")
     public String main() {
 
