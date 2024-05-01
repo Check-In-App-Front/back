6738ea0b82ab4fb320d7513d5d4702fc261ac5c6
parkJi-yun <nmjk0123@naver.com>
2024-05-01 16:08:08 +0900

    Rename: 프로젝트 파일 옮김

--- src/main/java/com/ace/project/ProjectApplication.java
+++ src/main/java/com/ace/project/ProjectApplication.java
@@ -2,21 +2,14 @@ package com.ace.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@RestController
+@EnableJpaAuditing
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-	
-	@GetMapping("/test")
-	public String hello(){
-		return "테스트입니다.";
-	}
-
 
 }
