b9b82fc3d3207ec3842188c13ce562184dcf6b96
parkJi-yun <nmjk0123@naver.com>
2024-05-02 00:52:05 +0900

    Style: 공백 삭제

--- src/main/java/com/ace/project/jwt/JWTFilter.java
+++ src/main/java/com/ace/project/jwt/JWTFilter.java
@@ -19,8 +19,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-
-
 @RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
 	
