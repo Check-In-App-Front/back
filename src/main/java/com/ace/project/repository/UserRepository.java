b41636dee1e69f9d8b0b87cc694d42aa72f4f2dd
parkJi-yun <nmjk0123@naver.com>
2024-05-02 00:13:16 +0900

    Feat: findByEmail

--- src/main/java/com/ace/project/repository/UserRepository.java
+++ src/main/java/com/ace/project/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ace.project.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	User findByEmail(String email);
 }
