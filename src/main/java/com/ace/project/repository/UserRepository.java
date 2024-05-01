2509fbb97bf1f7d243bab21849f60386b50bd8b5
parkJi-yun <nmjk0123@naver.com>
2024-05-02 00:59:26 +0900

    Refactor: @Refactor 추가

--- src/main/java/com/ace/project/repository/UserRepository.java
+++ src/main/java/com/ace/project/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ace.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ace.project.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	User findByEmail(String email);
 }
